73b0f75bf186b15f1a9cec77f70742bef06cd570
ARGENIS VARGAS MENA <argenis.vargas@ulatina.net>
2019-02-24 13:18:00 -0600

    Creación
    Creción del proyecto

---
+++ ProyectoPrograAvanzada/ProyectoPrograAvanzada/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoPrograAvanzada.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "Usuario")]
+        public string Users { get; set; }
+
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+    }
+}
